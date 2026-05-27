7a1ea68dfc7de786ad885e5b4d70a14ceddd9fb0
Vivreamajorque <amely.attias@gmail.com>
2026-05-27 12:16:16 +0200

    add: src/config.js

---
+++ src/config.js
@@ -0,0 +1,52 @@
+export const NOTION_DB = {
+  guides:    '060e103b-f00c-4be4-9b8a-b62f25479376',
+  cockpit:   '0d18d966-312e-4d01-8122-1ecf3acb5a12',
+  annuaire:  '8c8927e0-f3e8-4204-981f-dd9fbd264b7e',
+  lifestyle: 'e88c3862-00fc-41f7-89f3-ff827edfcf2b',
+  actus:     '47380cca-a90b-4ed4-adc2-c98d03780271',
+}
+
+export const PROFILS = [
+  {
+    id: 'reve',
+    label: 'Je rêve',
+    notion: 'Je rêve',
+    emoji: '🌅',
+    desc: 'Je découvre Majorque, pas encore décidé',
+    color: '#7BA05B',
+  },
+  {
+    id: 'installe',
+    label: "Je m'installe",
+    notion: "Je m'installe",
+    emoji: '📦',
+    desc: 'Déménagement planifié ou en cours',
+    color: '#E8B86D',
+  },
+  {
+    id: 'premiere',
+    label: '1ère année',
+    notion: 'Je vis ici 1ère année',
+    emoji: '🏡',
+    desc: 'Arrivé récemment, régularisation en cours',
+    color: '#C47A5A',
+  },
+  {
+    id: 'confirme',
+    label: 'Résident confirmé',
+    notion: 'Je vis ici confirmé',
+    emoji: '🌿',
+    desc: 'Installé, vie quotidienne à Majorque',
+    color: '#5A7A40',
+  },
+]
+
+export const OWNER_EMAIL = 'amely.attias@gmail.com'
+
+export const PREMIUM_PRICE = '14,90€/mois'
+export const PREMIUM_STRIPE_LINK = 'https://buy.stripe.com/REPLACE_ME'
+
+export const GUIDE_CATEGORIES = [
+  'Administratif','Logement','Travail','Santé','Famille',
+  'Argent','Voiture','Animaux','Déménagement','Vie pratique',
+]
