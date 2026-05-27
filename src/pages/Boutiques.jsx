58223537a83dd20d7a073df639aa37f4f3848dc5
Amely <amely.attias@gmail.com>
2026-05-27 19:47:56 +0000

    fix: URL Mallorca Chérie → mallorcacherie.netlify.app

--- src/pages/Boutiques.jsx
+++ src/pages/Boutiques.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const MALLORCA_CHERIE_URL = 'https://mallorcacherie.com'
+const MALLORCA_CHERIE_URL = 'https://mallorcacherie.netlify.app'
 
 const SCENARIOS = [
   {
